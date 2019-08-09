1193be7b622d90c498f9b29373362ffce0d0dfed
gotjon05 <gotjon05@localhost.localdomain>
2019-08-09 15:55:12 -0400

    aug9

--- C_Programming_Language/1_ch/functions/1.20_detab.c
+++ C_Programming_Language/1_ch/functions/1.20_detab.c
@@ -1,27 +1,33 @@
 /*
 Write a program detab that replaces tabs in the input with the proper number of blanks to space for the next tab stop
 
-	getline 
-	replacetabs function 
-		replaces tabs with blanks
-*
+
+	Read input and put store it in array
+	
+	walkthrough array and replace tabs with proper spaces
+
 */
 
 
 #include <stdio.h>
 #define MAXLINE 1000
+#define TAB 8
 
+
+ 
 int getlines(char line[], int maxline);
-void detab(char line[]);
+void detab(char line[], int array_size);
 
 
 int main()
 {
 	char line[MAXLINE]; 
+	int array_size;
 	
-	while(getlines(line, MAXLINE) > 0){
-		detab(line);
-
+	while(array_size = getlines(line, MAXLINE)){
+		detab(line, array_size);
+                printf("%s", line);
+		
 
 	}
 }
@@ -40,20 +46,16 @@ int getlines(char line[], int maxline)
 
 }	
 
-void detab(char line[])
+void detab(char line[], int array_size)
 {
 	int i;
-
-	while(line[i] != '\0'){	
+	for(i=0; i<array_size; i++){
 		if(line[i] == '\t'){
-			line[i] = "        ";	
-		}
-		++i;
+			line[i] = "        ";
+		
+		}		
 	}
-
 }
-
-
 
 
 
